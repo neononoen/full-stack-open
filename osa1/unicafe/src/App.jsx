355fdf569be53a3bfffb30b2b52d3a3b287740dd
neononoen <roosa.nenonen@outlook.com>
2025-12-04 10:29:55 +0200

    osa1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {setGood(good + 1)}
+  const handleNeutralClick = () => {setNeutral(neutral + 1)}
+  const handleBadClick = () => {setBad(bad + 1)}
+
+  return (
+    <div>
+      <h1> give feedback </h1>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <h1> statistics </h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = ({ text, value }) => 
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good+neutral+bad
+  const average = (good-bad)/all
+  const positive = (good/all)*100 + '%'
+
+  if (all == 0) {
+    return (
+      <div>
+        <p> No feedback given </p>
+      </div>
+    )
+  } else {
+    return (
+      <table>
+      <tbody>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={all} />
+      <StatisticLine text='average' value={average} />
+      <StatisticLine text='positive' value={positive} />
+      </tbody>
+      </table>
+  )
+}
+}
+
+export default App
